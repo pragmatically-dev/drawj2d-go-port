589af2831d11a59774683288bdaedf19dd01710b
Pragmatically-dev <pragmatically.dev@gmail.com>
2024-06-29 14:19:16 -0300

    Mayor refactoring and optimization by reducing syscalls

--- main.go
+++ main.go
@@ -13,9 +13,6 @@ import (
 	"strings"
 	"time"
 
-	ed "github.com/ernyoke/imger/edgedetection"
-	im "github.com/ernyoke/imger/imgio"
-	"github.com/ernyoke/imger/padding"
 	"github.com/fsnotify/fsnotify"
 	rp "github.com/pragmatically-dev/drawj2d-rm/remarkablepage"
 	_ "go.uber.org/automaxprocs"
@@ -32,35 +29,31 @@ type Config struct {
 
 func postRmDocToWebInterface(filepath, DirToSave string) {
 	//fmt.Println("Starting Conversion")
-	rp.LaplacianEdgeDetection(filepath, DirToSave)
+	rmData := rp.LaplacianEdgeDetection(filepath, DirToSave)
 
 	//fmt.Println("File testPNGConversion.rm generated successfully.")
 	rmFile := rp.GetFileNameWithoutExtension(filepath)
 	rmFile = fmt.Sprintf("%s/%s.rm", DirToSave, rmFile)
-	rmDocPath := rp.CreateRmDoc(rmFile, DirToSave)
-
-	go func() {
+	rmDocBuff, rmDocPath := rp.CreateRmDoc(rmFile, DirToSave, rmData)
+	if rmDocBuff == nil {
+		fmt.Println("Error al crear zip en memoria:")
+		return
+	}
+	go func(rmDocPath string, rmdocbuff *bytes.Buffer) {
 		url := "http://10.11.99.1"
-
-		file, err := os.Open(rmDocPath)
-		if err != nil {
-			fmt.Println("Error al abrir el archivo:", err)
-			return
-		}
-		defer file.Close()
 
 		var requestBody bytes.Buffer
 		writer := multipart.NewWriter(&requestBody)
 
 		// Create a form file field
-		part, err := writer.CreateFormFile("file", fp.Base(file.Name()))
+		part, err := writer.CreateFormFile("file", fp.Base(rmDocPath))
+
 		if err != nil {
 			fmt.Println("Error creating the form file field:", err)
 			return
 		}
 
-		// Copy the file content to the form field
-		_, err = io.Copy(part, file)
+		_, err = io.Copy(part, rmdocbuff)
 		if err != nil {
 			fmt.Println("Error copying the file content:", err)
 			return
@@ -94,7 +87,7 @@ func postRmDocToWebInterface(filepath, DirToSave string) {
 		defer client.CloseIdleConnections()
 
 		go deleteFile(rmDocPath)
-	}()
+	}(rmDocPath, rmDocBuff)
 
 }
 
@@ -174,50 +167,6 @@ func AppStart() {
 }
 
 func main() {
-	Test2()
-	//Test3()
-}
+	AppStart()
 
-func Test3() {
-	img, err := im.ImreadGray("image.png")
-	if err != nil {
-		rp.DebugPrint("Error opening the file:", err)
-		return
-	}
-	laplacianGray, _ := ed.LaplacianGray(img, padding.BorderReplicate, ed.K8)
-
-	rp.BuildBooleanMatrix(laplacianGray)
-
-}
-
-func Test2() {
-	rp.LaplacianEdgeDetection("/home/nieva/Proyectos/drawj2d-rm/image.png", "/home/nieva/Proyectos/drawj2d-rm/")
-
-	//fmt.Println("File testPNGConversion.rm generated successfully.")
-	rmFile := rp.GetFileNameWithoutExtension("/home/nieva/Proyectos/drawj2d-rm/image.png")
-	rmFile = fmt.Sprintf("%s/%s.rm", "/home/nieva/Proyectos/drawj2d-rm/", rmFile)
-	_ = rp.CreateRmDoc(rmFile, "/home/nieva/Proyectos/drawj2d-rm/")
-
-}
-
-func Test() {
-	file, err := os.Create("test.rm")
-	if err != nil {
-		fmt.Println("Error creating file:", err)
-		return
-	}
-	defer file.Close()
-
-	page := rp.NewReMarkablePage(file, 1872) // Asumiendo una altura de página de 1000 unidades
-
-	page.AddPixel(500, 500)
-
-	err = page.Export()
-	if err != nil {
-		fmt.Println("Error exporting page:", err)
-		return
-	}
-	_ = rp.CreateRmDoc("/home/nieva/Proyectos/drawj2d-rm/test.rm", "")
-
-	fmt.Println("File testRemarkablePageSmiley.rm generated successfully.")
 }
